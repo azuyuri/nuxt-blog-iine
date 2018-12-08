ec5bca5f75028205d0f3ea2cfe7a0bd5cebf165c
azuyuri <tamondojo@gmail.com>
2018-12-08 19:02:47 +0900

    fix: Finish posts and TheHeader

--- app/components/TheHeader.vue
+++ app/components/TheHeader.vue
@@ -1,20 +1,21 @@
 <template>
   <el-menu mode="horizontal" :router="true">
     <el-menu-item index="1" style="pointer-events:none;">
-      Nuxt Dairy App
+      Nuxt Diary App
     </el-menu-item>
     <el-menu-item index="2" :route="{ path: '/posts/' }">
       投稿一覧
     </el-menu-item>
 
-    <ei-menu-item index="4" style="float: right;"
-      :route="{ path: `/users/${user/id}` }" v-if="user">
-    <span>{{ user.id }}</span>
-    </ei-menu-item>
+    <no-ssr>
+      <el-menu-item index="4" style="float: right;" :route="{ path: `/users/${user.id}` }" v-if="user">
+        <span>{{ user.id }}</span>
+      </el-menu-item>
       <el-menu-item index="4" style="float: right;" :route="{ path: '/' }" v-else>
         <span>ログイン</span>
       </el-menu-item>
-    <el-menu-item index="5" style="float: right;" :route="{ path: '/posts/new' }">
+    </no-ssr>
+    <el-menu-item index="5" style="float: right" :route="{ path: '/posts/new' }">
       新規投稿
     </el-menu-item>
   </el-menu>
@@ -28,4 +29,3 @@ export default {
   }
 }
 </script>
-
--- app/pages/posts/new.vue
+++ app/pages/posts/new.vue
@@ -1,15 +1,15 @@
 <template>
-  <section>
-    <el-card>
-      <div>
-        <el-input />
+  <section class="container posts-page">
+    <el-card style="flex: 1">
+      <div slot="header" class="clearfix">
+        <el-input placeholder="タイトルを入力" v-model="formData.title" />
       </div>
       <div>
-        <el-input />
+        <el-input placeholder="本文を入力" type="textarea" rows="15" v-model="formData.body" />
       </div>
-      <div class="">
-        <el-button>
-          <span />
+      <div class="text-right" style="margin-top: 16px;">
+        <el-button type="primary" @click="publish" round>
+          <span class="el-icon-upload2" />
           <span>Publish</span>
         </el-button>
       </div>
@@ -19,13 +19,11 @@
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
-
 export default {
   asyncData({ redirect, store }) {
     if (!store.getters['user']) {
 redirect('/')
     }
-
     return {
       formData: {
         title: '',
@@ -36,18 +34,23 @@ redirect('/')
   computed: {
     ...mapGetters(['user'])
   },
-
   methods: {
-    publish() {
-    }
+    async publish() {
+      const payload = {
+        user: this.user,
+        ...this.formData
+      }
+      await this.publishPost({ payload })
+      this.$router.push('/posts')
+    },
+    ...mapActions('users', ['updateUser']),
+    ...mapActions('posts', ['publishPost'])
   }
 }
 </script>
 
 <style>
-.post-page .el-table__row {
+.posts-page .el-table__row {
   cursor: pointer;
 }
 </style>
-
-
