f50ae048de7062ab99c692f01645fd24e97ee509
azuyuri <tamondojo@gmail.com>
2018-12-08 18:21:31 +0900

    feat: Define moment.js

---
+++ app/pages/posts/new.vue
@@ -0,0 +1,53 @@
+<template>
+  <section>
+    <el-card>
+      <div>
+        <el-input />
+      </div>
+      <div>
+        <el-input />
+      </div>
+      <div class="">
+        <el-button>
+          <span />
+          <span>Publish</span>
+        </el-button>
+      </div>
+    </el-card>
+  </section>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  asyncData({ redirect, store }) {
+    if (!store.getters['user']) {
+redirect('/')
+    }
+
+    return {
+      formData: {
+        title: '',
+        body: ''
+      }
+    }
+  },
+  computed: {
+    ...mapGetters(['user'])
+  },
+
+  methods: {
+    publish() {
+    }
+  }
+}
+</script>
+
+<style>
+.post-page .el-table__row {
+  cursor: pointer;
+}
+</style>
+
+
