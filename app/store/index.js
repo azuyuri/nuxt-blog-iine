7c643a8b0a4b6bcfa9e03025f216f87249c13391
azuyuri <tamondojo@gmail.com>
2018-12-12 21:56:32 +0900

    test: Adding Jest and small test passing

--- app/store/index.js
+++ app/store/index.js
@@ -1,53 +1,26 @@
-import moment from '~/plugins/moment'
-export const state = () => ({
-  isLoggedIn: false,
-  user: null
+const state = () => ({
+  count: 0
 })
 
-//このブロックがなかったのでログインできなかった
-export const getters = {
-  isLoggedIn: state => state.isLoggedIn,
-  user: state => (state.user ? Object.assign({ likes: [] }, state.user) : null)
+const getters = {
+  count: (state) => state.count
 }
 
-export const mutations = {
-  setUser(state, { user }) {
-    state.user = user
-    state.isLoggedIn = true
+const mutations = {
+  increment(state) {
+    state.count++
   }
 }
 
-export const actions = {
-  //ログイン
-  async login({ commit }, { id }) {
-    const user = await this.$axios.$get(`/users/${id}.json`)
-    console.log(user)
-    if (!user.id) throw new Error('Invalid user')
-    commit('setUser', { user })
-  },
-  //register
-  async register({ commit }, { id }) {
-    const payload = {}
-    payload[id] = { id }
-    await this.$axios.$patch(`/users.json`, payload)
-    const user = await this.$axios.$get(`/users/${id}.json`)
-    if (!user.id) throw new Error('Invalid user')
-    commit('setUser', { user })
-  },
-  //いいね
-  async addLikeLogToUser({ commit }, { user, post }) {
-    user.likes.push({
-      created_at: moment().format(),
-      user_id: user.id,
-      post_id: post.id
-    })
-    const newUser = await this.$axios.$put(`/users/${user.id}.json`, user)
-    commit('updateUser', { user: newUser })
-  },
-  //いいね取り消し
-  async removeLikeLogToUser({ commit }, { user, post }) {
-    user.likes = post.likes.filter(like => like.user_id !== user.id) || []
-    const newUser = await this.$axios.$put(`/users/${user.id}.json`, user)
-    commit('updateUser', { user: newUser })
+const actions = {
+  increment({ commit }) {
+    commit('increment')
   }
+}
+
+module.exports = {
+  state,
+  getters,
+  mutations,
+  actions
 }
